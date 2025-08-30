100d1582071104c346a1fe305068329c4503c493
johann-gorban <ivangorbanwork@gmail.com>
2025-08-30 11:46:10 +0300

    Bug fixes and run script added

--- bot/services/config.py
+++ bot/services/config.py
@@ -3,3 +3,8 @@ from pathlib import Path
 ALERT_COOLDOWN_SEC = 600
 
 CHAT_IDS_PATH = Path(__file__).resolve().parent.parent.parent / 'data' / 'chat_ids.txt'
+
+CHAT_IDS_PATH.parent.mkdir(parents=True, exist_ok=True)
+
+if not CHAT_IDS_PATH.exists():
+    CHAT_IDS_PATH.touch()
---
+++ run.sh
@@ -0,0 +1,2 @@
+docker build -t xwatchdog .
+docker run -d xwatchdog
