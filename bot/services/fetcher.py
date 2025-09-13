3f39b50c59ca00d6e9101c60fb8ee262041166aa
johann-gorban <ivangorbanwork@gmail.com>
2025-09-14 00:01:27 +0300

    Fixed width fetching problemd

--- bot/services/fetcher.py
+++ bot/services/fetcher.py
@@ -1,5 +1,7 @@
 from playwright.async_api import async_playwright
+from typing import List
 import logging
+import re
 
 
 playwright_instance = None
@@ -24,6 +26,12 @@ async def close_fetcher():
 
 
 async def fetch_width(url: str) -> float | None:
+    global browser
+
+    if not browser or not browser.is_connected():
+        logging.warning("Browser is not available, reinitializing...")
+        await init_fetcher()
+
     logging.info(f"Start fetching width from {url}")
     page = await browser.new_page()
     try:
@@ -31,11 +39,11 @@ async def fetch_width(url: str) -> float | None:
         logging.info("Page loaded")
         await page.wait_for_selector(".h-\\[4px\\].rounded-full.relative.z-1")
 
-        width: str = await page.eval_on_selector(
+        style_str: str = await page.eval_on_selector(
             ".h-\\[4px\\].rounded-full.relative.z-1",
-            "el => el.style.width"
+            "el => el.getAttribute('style')"
         )
-        logging.info(f"Raw width from style: '{width}'")
+        logging.info(f"Full style attribute: {style_str}")
     except Exception as e:
         logging.error(f"Error fetching width: {e}")
         await page.close()
@@ -43,10 +51,9 @@ async def fetch_width(url: str) -> float | None:
 
     await page.close()
 
-    if width and width.endswith('%'):
-        value = float(width.rstrip('%'))
-        logging.info(f"Parsed width: {value}%")
-        return value
-    else:
-        logging.warning("Width is empty or not in percent format")
-        return None
+    matches: List[str] = re.findall(r'(\d+(?:\.\d+)?)%', style_str)
+    width: float = float(matches[-1].rstrip('%')) if matches else None
+
+    logging.info(f'Get width: {width}')
+
+    return width
